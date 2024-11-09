892358fbcd840f1f40ca8c305213e982d6b5b072
ChaykinDenis <chaichkin.denis@gmail.com>
2024-11-09 10:32:47 +0300

    rename

--- src/main/java/org/example/alfa/Application.java
+++ src/main/java/org/example/alfa/Application.java
@@ -8,7 +8,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class Application {
 
+
 	public static void main(String[] args) {
+		System.out.println();
 		SpringApplication.run(Application.class, args);
 	}
 }
